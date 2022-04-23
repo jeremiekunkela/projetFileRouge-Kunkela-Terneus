0e5e6b7441c01f471f7026c356fbe185d51b037e
jeremie <jeremiekunkela@gmail.com>
2022-04-23 20:27:52 +0200

    Suppresion du ndossier view et creation de homeView

---
+++ src/views/Home.vue
@@ -0,0 +1,9 @@
+<template>
+<h1>Home</h1>
+<li><router-link to="/">Accueil</router-link></li>
+
+<!-- Éléments statiques qui ne changent pas -->
+<!-- ... -->
+<!-- Partie dynamique -->
+<router-view></router-view>
+</template>
